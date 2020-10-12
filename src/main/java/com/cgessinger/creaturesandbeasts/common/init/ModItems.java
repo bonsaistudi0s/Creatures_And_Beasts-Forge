04a4d1d94d546463a5a387ab292706a5c487723a
CGessinger <christian@gessinger.de>
2020-10-12 14:35:02 +0200

    Add apple slices

--- src/main/java/com/cgessinger/creaturesandbeasts/common/init/ModItems.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/common/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.cgessinger.creaturesandbeasts.common.init;
 
 import com.cgessinger.creaturesandbeasts.CreaturesAndBeasts;
+import com.cgessinger.creaturesandbeasts.common.items.AppleSliceItem;
 import com.cgessinger.creaturesandbeasts.common.items.ModSpawnEggItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -16,4 +17,6 @@ public class ModItems
 	
 	public static RegistryObject<ModSpawnEggItem> LIZARD_SPAWN_EGG = ITEMS.register("lizard_spawn_egg",
 			() -> new ModSpawnEggItem(ModEntityTypes.LIZARD,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)));
+
+	public static final RegistryObject<AppleSliceItem> APPLE_SLICE = ITEMS.register("apple_slice", AppleSliceItem::new);
 }
