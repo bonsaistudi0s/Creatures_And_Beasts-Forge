9d48bd60a0a38f48d72bdb46bbdde0e97fbdeffa
CGessinger <christian@gessinger.de>
2020-10-25 12:20:58 +0100

    Add net durability

--- src/main/java/com/cgessinger/creaturesandbeasts/common/items/EntityNetItem.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/common/items/EntityNetItem.java
@@ -15,24 +15,35 @@ public class EntityNetItem extends Item
 	{
 		super(new Item.Properties()
 				.group(CreaturesAndBeasts.TAB)
+				.maxDamage(64)
 		);
 	}
 
 	@Override
-	public ActionResultType itemInteractionForEntity (ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand)
+	public ActionResultType itemInteractionForEntity (ItemStack stackIn, PlayerEntity playerIn, LivingEntity target, Hand hand)
 	{
 		if(target instanceof IModNetable && target.isAlive())
 		{
 			IModNetable entity = (IModNetable) target;
-			ItemStack itemstack = entity.getItem();
-			if(itemstack != null)
+			ItemStack entityItem = entity.getItem();
+			if(entityItem != null)
 			{
-				playerIn.addItemStackToInventory(itemstack);
+				playerIn.addItemStackToInventory(entityItem);
 				entity.spawnParticleFeedback();
 				target.remove();
+				if (!playerIn.world.isRemote()) {
+					stackIn.damageItem(1, playerIn, (p_220000_1_) -> {
+						p_220000_1_.sendBreakAnimation(hand);
+					});
+				}
 				return ActionResultType.SUCCESS;
 			}
 		}
 		return ActionResultType.FAIL;
+	}
+
+	@Override
+	public int getItemEnchantability() {
+		return 1;
 	}
 }
