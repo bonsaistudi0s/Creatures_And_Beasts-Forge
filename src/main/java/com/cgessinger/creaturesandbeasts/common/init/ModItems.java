f59a6a9bdc455c098b2da54d34c9fb2b331a0f37
CGessinger <christian@gessinger.de>
2020-11-08 15:33:53 +0100

    Update little grebe spawn egg texture

--- src/main/java/com/cgessinger/creaturesandbeasts/common/init/ModItems.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/common/init/ModItems.java
@@ -17,7 +17,7 @@ public class ModItems
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CreaturesAndBeasts.MOD_ID);
 
 	public static RegistryObject<ModSpawnEggItem> GREBE_SPAWN_EGG = ITEMS.register("little_grebe_spawn_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.LITTLE_GREBE,0x201d1c, 0x6d3021, new Item.Properties().group(CreaturesAndBeasts.TAB)));
+			() -> new ModSpawnEggItem(ModEntityTypes.LITTLE_GREBE,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)));
 	
 	public static RegistryObject<ModSpawnEggItem> LIZARD_SPAWN_EGG = ITEMS.register("lizard_spawn_egg",
 			() -> new ModSpawnEggItem(ModEntityTypes.LIZARD,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)));
