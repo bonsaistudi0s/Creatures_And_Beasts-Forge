f5d6a5456fbe99e6c84747e1654956c503597701
CGessinger <christian@gessinger.de>
2020-12-13 16:56:11 +0100

    Fix sporeling item textures

--- src/main/java/com/cgessinger/creaturesandbeasts/common/init/ModItems.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/common/init/ModItems.java
@@ -42,22 +42,22 @@ public class ModItems
 
 	// Sporelings
 	public static RegistryObject<ModSpawnEggItem> SPORELING_OVERWORLD_BROWN_EGG = ITEMS.register("sporeling_overworld_brown_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.FRIENDLY_SPORELING,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(0));
+			() -> new ModSpawnEggItem(ModEntityTypes.FRIENDLY_SPORELING,0xF2DDD3, 0xCD9777, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(0));
 
 	public static RegistryObject<ModSpawnEggItem> SPORELING_OVERWORLD_RED_EGG = ITEMS.register("sporeling_overworld_red_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.FRIENDLY_SPORELING,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(1));
+			() -> new ModSpawnEggItem(ModEntityTypes.FRIENDLY_SPORELING,0xCB1A1A, 0xF2DDD3, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(1));
 
 	public static RegistryObject<ModSpawnEggItem> SPORELING_NETHER_BROWN_EGG = ITEMS.register("sporeling_nether_brown_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.HOSTILE_SPORELING,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(2));
+			() -> new ModSpawnEggItem(ModEntityTypes.HOSTILE_SPORELING,0xC68F70, 0x7C533F, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(2));
 
 	public static RegistryObject<ModSpawnEggItem> SPORELING_NETHER_RED_EGG = ITEMS.register("sporeling_nether_red_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.HOSTILE_SPORELING,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(3));
+			() -> new ModSpawnEggItem(ModEntityTypes.HOSTILE_SPORELING,0xCC9575, 0xB31A1A, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(3));
 
 	public static RegistryObject<ModSpawnEggItem> SPORELING_WARPED_FUNGI_EGG = ITEMS.register("sporeling_warped_fungi_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.NEUTRAL_SPORELING,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(4));
+			() -> new ModSpawnEggItem(ModEntityTypes.NEUTRAL_SPORELING,0x139A88, 0xD66818, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(4));
 
 	public static RegistryObject<ModSpawnEggItem> SPORELING_CRIMSON_FUNGUS_EGG = ITEMS.register("sporeling_crimson_fungus_egg",
-			() -> new ModSpawnEggItem(ModEntityTypes.NEUTRAL_SPORELING,0x00FFFFFF, 0x00FFFFFF, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(5));
+			() -> new ModSpawnEggItem(ModEntityTypes.NEUTRAL_SPORELING,0x9E1104, 0xF88539, new Item.Properties().group(CreaturesAndBeasts.TAB)).spawnsVariant(5));
 
 	static {
 		LIZARD_SPAWN_MAP.put(0, LIZARD_ITEM_0);
