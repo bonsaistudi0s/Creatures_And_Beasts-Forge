09dabace024f43a1b712dc36a3e27fcc4ad2a06d
HellionGames <dsgillespie02@yahoo.com>
2022-06-08 19:18:20 -0400

    Changed Redirects to ModifyVariable for safer compatibility

--- src/main/java/com/cgessinger/creaturesandbeasts/mixin/MixinGeoEntityRenderer.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/mixin/MixinGeoEntityRenderer.java
@@ -1,23 +1,25 @@
 package com.cgessinger.creaturesandbeasts.mixin;
 
 import com.cgessinger.creaturesandbeasts.entities.SporelingEntity;
-import net.minecraft.world.entity.Entity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 @Mixin(GeoEntityRenderer.class)
-public class MixinGeoEntityRenderer {
+public class MixinGeoEntityRenderer<T extends LivingEntity & IAnimatable> {
 
-    @Redirect(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getVehicle()Lnet/minecraft/world/entity/Entity;", ordinal = 2), remap = false)
-    private Entity CNB_stopSporelingRotatingOnPlayer(LivingEntity entity) {
+    @ModifyVariable(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "STORE"), remap = false)
+    private boolean CNB_stopSporelingRotatingOnPlayer(boolean value, T entity, float entityYaw, float partialTicks, PoseStack stack, MultiBufferSource bufferIn, int packedLightIn) {
         if (entity instanceof SporelingEntity && entity.getVehicle() instanceof Player) {
-            return null;
+            return false;
         } else {
-            return entity.getVehicle();
+            return value;
         }
     }
 }
