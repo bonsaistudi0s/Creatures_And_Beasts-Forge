97a6c41458e0a87b4d8b19cd77f6f6800a511b24
HellionGames <dsgillespie02@yahoo.com>
2022-06-09 13:57:36 -0400

    Made Minipad flower still glow when optifine installed

--- src/main/java/com/cgessinger/creaturesandbeasts/client/entity/render/MinipadGlowLayer.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/client/entity/render/MinipadGlowLayer.java
@@ -7,35 +7,36 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.ModList;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 @OnlyIn(Dist.CLIENT)
-public class MinipadGlowLayer extends GeoLayerRenderer {
+public class MinipadGlowLayer extends GeoLayerRenderer<MinipadEntity> {
     private static final ResourceLocation MINIPAD_MODEL = new ResourceLocation(CreaturesAndBeasts.MOD_ID, "geo/entity/minipad/minipad.geo.json");
 
-    public MinipadGlowLayer(IGeoRenderer entityRendererIn) {
+    public MinipadGlowLayer(IGeoRenderer<MinipadEntity> entityRendererIn) {
         super(entityRendererIn);
     }
 
     @Override
-    public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, Entity entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+    public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, MinipadEntity entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         long time = entityLivingBaseIn.level.getDayTime();
 
-        if (entityLivingBaseIn instanceof MinipadEntity minipadEntity && minipadEntity.isGlowing()) {
+        if (entityLivingBaseIn.isGlowing()) {
             RenderType eyesTexture = RenderType.entityTranslucent(new ResourceLocation(CreaturesAndBeasts.MOD_ID, "textures/entity/minipad/minipad_eyes_glow.png"));
 
-            RenderType flowerGlow = RenderType.eyes(minipadEntity.getMinipadType().getGlowTextureLocation());
-            RenderType flowerTranslucent = RenderType.entityTranslucent(minipadEntity.getMinipadType().getGlowTextureLocation());
+            RenderType flowerGlow = RenderType.eyes(entityLivingBaseIn.getMinipadType().getGlowTextureLocation());
+            RenderType flowerTranslucent = RenderType.entityTranslucent(entityLivingBaseIn.getMinipadType().getGlowTextureLocation());
 
             matrixStackIn.pushPose();
 
             this.getRenderer().render(this.getEntityModel().getModel(MINIPAD_MODEL), entityLivingBaseIn, partialTicks, flowerGlow, matrixStackIn, bufferIn, bufferIn.getBuffer(flowerGlow), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, 0.0f);
-            this.getRenderer().render(this.getEntityModel().getModel(MINIPAD_MODEL), entityLivingBaseIn, partialTicks, flowerTranslucent, matrixStackIn, bufferIn, bufferIn.getBuffer(flowerTranslucent), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, (float) Math.pow((time-18000)/5000f, 2));
-
+            if (!ModList.get().isLoaded("optifine")) {
+                this.getRenderer().render(this.getEntityModel().getModel(MINIPAD_MODEL), entityLivingBaseIn, partialTicks, flowerTranslucent, matrixStackIn, bufferIn, bufferIn.getBuffer(flowerTranslucent), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, (float) Math.pow((time - 18000) / 5000f, 2));
+            }
             this.getRenderer().render(this.getEntityModel().getModel(MINIPAD_MODEL), entityLivingBaseIn, partialTicks, eyesTexture, matrixStackIn, bufferIn, bufferIn.getBuffer(eyesTexture), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, (float) -Math.pow((time-18000)/5000f, 2) + 1);
 
             matrixStackIn.popPose();
