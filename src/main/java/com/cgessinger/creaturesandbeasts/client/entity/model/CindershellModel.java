87161469f382469e319cb0eeb496a099e3e1db7f
HellionGames <dsgillespie02@yahoo.com>
2022-06-25 13:42:43 -0400

    Fixed entity heads spinning while paused

--- src/main/java/com/cgessinger/creaturesandbeasts/client/entity/model/CindershellModel.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/client/entity/model/CindershellModel.java
@@ -2,10 +2,12 @@ package com.cgessinger.creaturesandbeasts.client.entity.model;
 
 import com.cgessinger.creaturesandbeasts.CreaturesAndBeasts;
 import com.cgessinger.creaturesandbeasts.entities.CindershellEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.model.provider.data.EntityModelData;
@@ -48,8 +50,12 @@ public class CindershellModel extends AnimatedGeoModel<CindershellEntity> {
         IBone head = this.getAnimationProcessor().getBone("head");
 
         EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
-        head.setRotationX(head.getRotationX() + extraData.headPitch * ((float) Math.PI / 180F));
-        head.setRotationY(head.getRotationY() + extraData.netHeadYaw * ((float) Math.PI / 180F));
+
+        AnimationData manager = entity.getFactory().getOrCreateAnimationData(uniqueID);
+        int unpausedMultiplier = !Minecraft.getInstance().isPaused() || manager.shouldPlayWhilePaused ? 1 : 0;
+
+        head.setRotationX(head.getRotationX() + extraData.headPitch * ((float) Math.PI / 180F) * unpausedMultiplier);
+        head.setRotationY(head.getRotationY() + extraData.netHeadYaw * ((float) Math.PI / 180F) * unpausedMultiplier);
     }
 
 }
