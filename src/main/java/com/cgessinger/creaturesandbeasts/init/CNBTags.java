ec698623cd93384cef783556b29aa55fd66c0929
Xaidee <xaideems@gmail.com>
2022-07-27 13:47:23 +1200

    Made all breed/tame items rely on tags instead of hard-coded items because I wanna be able to tame my end whales with bolloom fruit in my modpack, this feature will probably be useful for others too.
    Using the same format as the tag used by vanilla's Foxes which utilise the "minecraft:fox_food" tag:
    "cnb:cindershell_food"
    "cnb:end_whale_food"
    "cnb:little_grebe_food"
    "cnb:sporeling_food"

--- src/main/java/com/cgessinger/creaturesandbeasts/init/CNBTags.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/init/CNBTags.java
@@ -10,6 +10,10 @@ public class CNBTags {
     public static class Items {
         public static final TagKey<Item> MINIPAD_FLOWERS = tag("minipad_flowers");
         public static final TagKey<Item> GLOWING_MINIPAD_FLOWERS = tag("glowing_minipad_flowers");
+        public static final TagKey<Item> CINDERSHELL_FOOD = tag("cindershell_food");
+        public static final TagKey<Item> END_WHALE_FOOD = tag("end_whale_food");
+        public static final TagKey<Item> LITTLE_GREBE_FOOD = tag("little_grebe_food");
+        public static final TagKey<Item> SPORELING_FOOD = tag("sporeling_food");
 
         private static TagKey<Item> tag(String name) {
             return ItemTags.create(new ResourceLocation(CreaturesAndBeasts.MOD_ID, name));
