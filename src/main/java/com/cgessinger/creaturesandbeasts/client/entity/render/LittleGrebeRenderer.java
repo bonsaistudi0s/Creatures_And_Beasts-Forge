d250739b53b444717609ac0c90ccab58dc3e6217
Darien Gillespie <dsgillespie02@yahoo.com>
2022-11-03 08:12:43 -0400

    Fixed Little Grebe having wrong render type

--- src/main/java/com/cgessinger/creaturesandbeasts/client/entity/render/LittleGrebeRenderer.java
+++ src/main/java/com/cgessinger/creaturesandbeasts/client/entity/render/LittleGrebeRenderer.java
@@ -2,9 +2,15 @@ package com.cgessinger.creaturesandbeasts.client.entity.render;
 
 import com.cgessinger.creaturesandbeasts.client.entity.model.LittleGrebeModel;
 import com.cgessinger.creaturesandbeasts.entities.LittleGrebeEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 @OnlyIn(Dist.CLIENT)
@@ -15,4 +21,8 @@ public class LittleGrebeRenderer extends GeoEntityRenderer<LittleGrebeEntity> {
         this.shadowRadius = 0.4F;
     }
 
+    @Override
+    public RenderType getRenderType(LittleGrebeEntity animatable, float partialTicks, PoseStack stack, @Nullable MultiBufferSource renderTypeBuffer, @Nullable VertexConsumer vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+        return RenderType.entityCutoutNoCull(textureLocation);
+    }
 }
